0d15378d36ccc26f732bb9b0644647e4b8cd01cb
Hees1989 <Hees1989@users.noreply.github.com>
2020-04-15 13:21:33 +0200

    comments

--- tensorflow/linear_regression_tensorflow.py
+++ tensorflow/linear_regression_tensorflow.py
@@ -1,26 +1,31 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np  # import for easy use of n-dimensional arrays
+import pandas as pd  # import for use of dataframe
+import matplotlib.pyplot as plt  # plot shows data in graph
 from six.moves import urllib
 
 import tensorflow.compat.v2.feature_column as fc
 
 import tensorflow as tf
 
+# Read csv file and only use columns on given indexes
 raw_data = pd.read_csv("student-mat.csv", sep=";")
 raw_data = raw_data[
     ["school", "sex", "age", "Mjob", "Fjob", "traveltime",
      "studytime", "failures", "absences", "G1", "G2", "G3"]
 ]
+# use 80% of data as training dataframe
 dftrain = raw_data.sample(frac=0.8, random_state=0)
 print(dftrain[:10])
+# use remaining data as evaluation (testing) dataframe
 dfeval = raw_data.drop(dftrain.index)
+# remove value that needs to be predicted (labels)
 y_train = dftrain.pop('G3')
 print(y_train[:10])
 y_eval = dfeval.pop('G3')
 
+# Use feature columns to map all possible outputs
 CATEGORICAL_COLUMNS = ['school', 'sex', 'Mjob', 'Fjob']
 
 NUMERIC_COLUMNS = ['age', 'traveltime', 'studytime', 'failures', 'absences', 'G1', 'G2']
@@ -35,10 +40,12 @@ for feature_name in NUMERIC_COLUMNS:
 
 print(feature_columns)
 
+# Define how data is broken into batches and epochs to feed to model.
+
 
 def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
-    def input_function():
-        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
+    def input_function():  # function that will be returned
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create dataset with dictionary of data and label
         if shuffle:
             ds = ds.shuffle(1000)
         ds = ds.batch(batch_size).repeat(num_epochs)
@@ -49,13 +56,14 @@ def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32)
 train_input_fn = make_input_fn(dftrain, y_train)
 eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
 
-# Voor classificatie evt.
+# for classification (binary results (chance of failing/succeeding)
 # linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
 
+# train linear regression model to predict label value based on feature values
 linear_est = tf.estimator.LinearRegressor(feature_columns=feature_columns)
 
-linear_est.train(train_input_fn)
-result = linear_est.evaluate(eval_input_fn)
+linear_est.train(train_input_fn)  # training..
+result = linear_est.evaluate(eval_input_fn)  # result with predictions
 
 dfeval['G3'] = y_eval
 dfeval['prediction'] = list(linear_est.predict(eval_input_fn))

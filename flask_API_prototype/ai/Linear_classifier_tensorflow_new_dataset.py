e677c5f6f4b8b0849129c83e890bb0cb959e681f
Jesse Koerhuis <JJA.Koerhuis@student.han.nl>
2020-05-20 11:14:20 +0200

    Fix voor Matthijs test

--- flask_API_prototype/ai/Linear_classifier_tensorflow_new_dataset.py
+++ flask_API_prototype/ai/Linear_classifier_tensorflow_new_dataset.py
@@ -66,7 +66,7 @@ def get_ai_results(student_id):
     print("Real result:", eval_column.loc[46])
     print("Prediction", result[46]['probabilities'][1])
 
-    if student_id in result:
+    if 0 <= student_id < len(result):
         return str(result[student_id]['probabilities'][1])
     else:
         return None
